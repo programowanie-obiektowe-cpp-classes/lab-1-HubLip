cc1b804d2169bfe635728c816115c24157e08c98
HubLip <01171666@pw.edu.pl>
2024-10-19 13:01:47 +0200

    Zejsciowka1 Proba1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,42 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x;
+    double y;
+
+public:
+    Wektor2D() : x(0), y(0) {}
+
+    Wektor2D (double xVal, double yVal) : x(xVal), y(yVal) {}
+
+    void setX (double xVal)
+    {
+        x = xVal;
+    }
+
+    void setY (double yVal)
+    {
+        y = yVal;
+    }
+
+    double getX ()
+    {
+        return x;
+    }
+
+    double getY ()
+    {
+        return y;
+    }
+
+    Wektor2D operator+ (Wektor2D& A)
+    {
+        return Wektor2D(x + A.x, y + A.y);
+    }
+
+    double operator* (Wektor2D& A)
+    {
+        return x * A.x + y * A.y;
+    }
+
 };
